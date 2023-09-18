9b30fb3081668b03b72f2f91400191b1da6cceab
Alice088 <alien280579@gmail.com>
2023-09-18 18:48:00 +0300

    delete karma

---
+++ tests/unit/LoginIn.spec.ts
@@ -0,0 +1,47 @@
+import { expect } from "chai";
+
+function ValidationPassword(inputText: string): boolean {
+  const isInvalidInputText = !inputText || inputText.toString().length > 20;
+
+  return !isInvalidInputText;
+}
+
+function makePassword() {
+  let text = ``;
+
+  for (let i = 0; i < 20; i++) {
+    text += `${String.fromCodePoint(
+      Math.floor(Math.random() * (220 - 20) + 50)
+    )}`;
+
+    text += `${Math.floor(Math.random() * (220 - 20) + 50)}`;
+  }
+  return text.slice(0, 20);
+}
+
+function makeLogin() {
+  let text = ``;
+
+  for (let i = 0; i < 20; i++) {
+    text += `${String.fromCodePoint(
+      Math.floor(Math.random() * (220 - 20) + 50)
+    )}`;
+  }
+  return text;
+}
+
+describe("check form password", () => {
+  it("Get password and checking it", () => {
+    for (let i = 0; i < 500; i++) {
+      expect(ValidationPassword(makePassword())).equal(true);
+    }
+  });
+});
+
+describe("check form login", () => {
+  it("Get login and checking it", () => {
+    for (let i = 0; i < 500; i++) {
+      expect(ValidationPassword(makeLogin())).equal(true);
+    }
+  });
+});
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  chainWebpack: (config) => {
-    config.module
-      .rule("ts")
-      .use("ts-loader")
-      .loader("ts-loader")
-      .tap((opts) => {
-        opts.appendTsSuffixTo = [/\.vue$/];
-        return opts;
-      });
-  },
 });
